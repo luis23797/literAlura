abe82e662a329356195313549a118488bdb7ad25
Luis <luis23797@gmail.com>
2025-06-04 12:58:01 -0600

    Inicio de LiterAlura

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import com.alura.literAlura.service.ConsumoAPI;
+
+public class Main {
+    private ConsumoAPI consumoAPI = new ConsumoAPI();
+
+    public void mostrarMenu(){
+        System.out.println("""
+                1 - Buscar libro
+                2 - 
+                
+                
+                """);
+    }
+
+}
